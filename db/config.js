bd01250bbe781a6f54d731bb0a974c44562ce802
Madison Crocker <m.voltaire.c@gmail.com>
2018-01-16 12:30:09 -0500

    fixing config issues

--- db/config.js
+++ db/config.js
@@ -1,7 +1,37 @@
 const Sequelize = require('sequelize');
 // const CLEARDB_DATABASE_URL = 'mysql://b03e5e18f3e601:b92c6c39@us-cdbr-iron-east-05.cleardb.net/heroku_4d344af8cb59e0f?reconnect=true'
 
+<<<<<<< HEAD
 const sequelize = new Sequelize('heroku_4d344af8cb59e0f', 'b03e5e18f3e601', 'b92c6c39', {
+=======
+
+
+
+//             match = process.env.CLEARDB_DATABASE_URL.match(/mysql:\/\/([^:]+):([^@]+)@([^:]+)\/(.+)\?/);
+            
+//             config = {
+//                 user: match[1],
+//                 pass: match[2],
+//                 base: match[4],
+//                 options: {
+//                     dialect: 'mysql',
+//                     protocol: 'mysql',
+//                     host: match[3],
+//                     port: 3306,
+//                     logging: false,
+//                     dialectOptions: {
+//                         ssl: true
+//                     }
+//                 }
+//             };
+            
+
+
+//         const sequelize = new Sequelize(config.base, config.user, config.pass, config.options)
+
+
+ const sequelize = new Sequelize('heroku_4d344af8cb59e0f', 'b03e5e18f3e601', 'b92c6c39', {
+>>>>>>> changes to config and debugged helper.js
   host: 'us-cdbr-iron-east-05.cleardb.net',
   dialect: 'mysql',
   pool: {

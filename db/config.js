ca76d8afe608b843bd5e44217f2fd68377922c34
Madison Crocker <m.voltaire.c@gmail.com>
2018-01-16 12:31:26 -0500

    fixing merge conflicts

--- db/config.js
+++ db/config.js
@@ -1,9 +1,6 @@
 const Sequelize = require('sequelize');
 // const CLEARDB_DATABASE_URL = 'mysql://b03e5e18f3e601:b92c6c39@us-cdbr-iron-east-05.cleardb.net/heroku_4d344af8cb59e0f?reconnect=true'
 
-<<<<<<< HEAD
-const sequelize = new Sequelize('heroku_4d344af8cb59e0f', 'b03e5e18f3e601', 'b92c6c39', {
-=======
 
 
 
@@ -31,7 +28,6 @@ const sequelize = new Sequelize('heroku_4d344af8cb59e0f', 'b03e5e18f3e601', 'b92
 
 
  const sequelize = new Sequelize('heroku_4d344af8cb59e0f', 'b03e5e18f3e601', 'b92c6c39', {
->>>>>>> changes to config and debugged helper.js
   host: 'us-cdbr-iron-east-05.cleardb.net',
   dialect: 'mysql',
   pool: {

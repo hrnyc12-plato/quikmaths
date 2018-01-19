05db6c5d1142a4338445c6b883dc9c7c05b26566
rvcwhitworth <rvcwhitworth@gmail.com>
2018-01-19 14:39:49 -0500

    (feat) initial setup for chatroom and styling

---
+++ client/src/components/chatRoom.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Paper from 'material-ui/Paper';
+
+class ChatRoom extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <Paper zDepth={2}>
+        THIS IS A PLACEHOLDER FOR THE CHAT
+      </Paper>
+    );
+  }
+}
+
+export default ChatRoom
